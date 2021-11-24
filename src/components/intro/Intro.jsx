916173d2e12efae8ac38ad8785a2651b35a5c59b
bishal regmi <bishalregmi180@gmail.com>
2021-11-24 13:21:10 +0545

    first commitgit

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,17 @@
+import "./intro.scss"
+
+export default function Intro() {
+    return (
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imagecontainer">
+
+                </div>
+            </div>
+            <div className="right"></div>
+            
+        
+            
+        </div>
+    )
+}
